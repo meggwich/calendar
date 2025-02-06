3ab92e74d74555ace6fdac643e8fae9d988aa8c9
Mikita <dixtry3826@gmail.com>
2025-02-06 22:41:43 +0100

    calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import moment from 'moment';
+moment.locale('ru');
+import PropTypes from 'prop-types';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Calendar = ({ date }) => {
+  const currentDate = moment(date);
+  
+  const generateCalendar = () => {
+    const startOfMonth = currentDate.clone().startOf('month');
+    const endOfMonth = currentDate.clone().endOf('month');
+    
+    const startWeek = startOfMonth.clone().startOf('week').isoWeekday(1);
+    const endWeek = endOfMonth.clone().endOf('week').isoWeekday(7);
+
+    const calendar = [];
+    let day = startWeek.clone();
+
+    while (day.isBefore(endWeek) || day.isSame(endWeek, 'day')) {
+      const week = [];
+      for (let i = 0; i < 7; i++) {
+        week.push({
+          date: day.clone(),
+          isCurrentMonth: day.isSame(currentDate, 'month'),
+          isToday: day.isSame(moment(date), 'day')
+        });
+        day.add(1, 'day');
+      }
+      calendar.push(week);
+    }
+    
+    return calendar;
+  };
+
+  const calendarWeeks = generateCalendar();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="ui-datepicker">
+      <div className="ui-datepicker-material-header">
+        <div className="ui-datepicker-material-day">
+          {currentDate.format('dddd')}
+        </div>
+        <div className="ui-datepicker-material-date">
+          <div className="ui-datepicker-material-day-num">
+            {currentDate.format('D')}
+          </div>
+          <div className="ui-datepicker-material-month">
+            {currentDate.format('MMMM')}
+          </div>
+          <div className="ui-datepicker-material-year">
+            {currentDate.format('YYYY')}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="ui-datepicker-header">
+        <div className="ui-datepicker-title">
+          <span className="ui-datepicker-month">
+            {currentDate.format('MMMM')}
+          </span>&nbsp;
+          <span className="ui-datepicker-year">
+            {currentDate.format('YYYY')}
+          </span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+      <table className="ui-datepicker-calendar">
+        <colgroup>
+          <col />
+          <col />
+          <col />
+          <col />
+          <col />
+          <col className="ui-datepicker-week-end" />
+          <col className="ui-datepicker-week-end" />
+        </colgroup>
+        <thead>
+          <tr>
+            {[...Array(7)].map((_, i) => (
+              <th key={i} scope="col" title={moment().isoWeekday(i+1).format('dddd')}>
+                {moment().isoWeekday(i + 1).format('dd')}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {calendarWeeks.map((week, weekIndex) => (
+            <tr key={weekIndex}>
+              {week.map((day, dayIndex) => {
+                const className = [
+                  !day.isCurrentMonth && 'ui-datepicker-other-month',
+                  day.isToday && 'ui-datepicker-today'
+                ].filter(Boolean).join(' ');
+                
+                return (
+                  <td key={dayIndex} className={className || undefined}>
+                    {day.date.format('D')}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+
+
+Calendar.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+};
+
+const now = new Date(2017, 2, 8);
+
+function App() {
+  return (
+    <Calendar date={now} />
+  );
 }
 
-export default App
+export default App;
+
